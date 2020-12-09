7c8aa21ce90d1186b59c121f402be1f58ff6e6ab
Marisol <19170157@uttcampus.edu.mx>
2020-12-09 01:36:03 -0600

    nuevas pantallas

---
+++ app/src/main/java/com/example/rmiroproyect/Luces.java
@@ -0,0 +1,14 @@
+package com.example.rmiroproyect;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Luces extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_luces);
+    }
+}
